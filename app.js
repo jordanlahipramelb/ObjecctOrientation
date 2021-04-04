abaa8f50953dec55d00b30437bb6ab648b28cb96
jordanlahipramelb <jordan.lahipramelb@gmail.com>
2021-04-04 16:17:07 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,96 @@
+// OO Challenge
+
+//PART 1
+
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  honk() {
+    return 'Beep';
+  }
+  toString() {
+    return `The vehicle is a ${this.make} ${this.model} from ${this.year}`;
+  }
+}
+
+/////////////////////////////////////////////////////////////////
+//PART 2
+
+class Car extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 4;
+  }
+}
+
+//OR
+
+// class Car extends Vehicle {
+//   constructor(make, model, year) {
+//     super(make, model, year);
+//   }
+//   numWheels() {
+//     return 4;
+//   }
+// }
+
+////////////////////////////////////////////////////////////////
+//PART 3
+
+class Motorcycle extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+  }
+
+  revEngine() {
+    return 'VROOM!!!';
+  }
+}
+
+//OR
+
+// class Motorcycle extends Vehicle {
+//   constructor(make, model, year) {
+//     super(make, model, year);
+//   }
+//   numWheels() {
+//     return 2;
+//   }
+//   revEngine() {
+//     return 'VROOM!!!';
+//   }
+// }
+
+//////////////////////////////////////////////////////////////////////
+
+//PART 4
+// Create a class for a Garage
+// It should have a property called vehicles which will store an array of vehicles,
+// and a property called capacity which is a number indicating how many vehicles will fit in the garage
+// When you create a garage, vehicles will always be empty; you only need to provide the capacity.
+
+// A garage should also have an add method, which attempts to add a vehicle to the array of vehicles.
+// However, if you try to add something which is not a vehicle, the garage should return the message “Only vehicles are allowed in here!”.
+// Also, if the garage is at capacity, it should say “Sorry, we’re full.”
+
+class Garage {
+  constructor(capacity) {
+    this.vehicles = [];
+    this.capacity = capacity;
+  }
+
+  add(newVehicle) {
+    if (!(newVehicle instanceof Vehicle)) {
+      return 'Only vehicles are allowed in here!';
+    }
+    if (this.vehicles.length >= this.capacity) {
+      return "Sorry, we're full.";
+    }
+    this.vehicles.push(newVehicle);
+    return 'Vehicle added!';
+  }
+}
